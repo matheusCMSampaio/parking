9e4b2bb6a66ce7fbea90135aa19d8c5ca3c446a0
Matheus Sampaio <86751496+matheusCMSampaio@users.noreply.github.com>
2023-09-13 00:15:28 -0300

    Update Conexao.java

--- src/br/com/fiap/model/dao/Conexao.java
+++ src/br/com/fiap/model/dao/Conexao.java
@@ -12,8 +12,8 @@ public class Conexao {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-			final String USER = "rm550489";
-			final String PASS = "140704";
+			final String USER = "";
+			final String PASS = "";
 			con = DriverManager.getConnection(url, USER, PASS);
 			System.out.println("Conexao aberta");
 		} catch (ClassNotFoundException e) {
